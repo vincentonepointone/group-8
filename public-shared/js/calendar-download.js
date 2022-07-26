fcc7590baa78fcd60f472c8f701701de60fcfefa
Vincent du Plessis <vinduples@gmail.com>
2022-07-26 21:58:16 +0200

    seperate js file for calendar download

---
+++ public-shared/js/calendar-download.js
@@ -0,0 +1,17 @@
+function takeshot() {
+    let div = document.getElementById('photo');
+
+    // Use the html2canv function to take a screen and append to the output div
+    html2canvas(div).then(
+        function (canvas) {
+         
+            let dataURl = canvas.toDataURL();
+            var link = document.createElement("a");
+            link.download = "rooster";
+            link.href = dataURl;
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+            delete link;
+        })    
+}
